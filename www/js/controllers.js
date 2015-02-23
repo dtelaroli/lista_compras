fdfa855aad7f88bec4bdcd56333af4a7793f3ed2
dtelaroli <ddts80@gmail.com>
2015-02-23 13:07:41 -0300

    Makes lists and product list

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,15 +2,50 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
+.controller('ListsCtrl', function($scope, Lists) {
+  var self = {
+    init: function() {
+      $scope.lists = Lists.all();
+      $scope.list = {};
+    }
+  };
+  self.init();
+
+  $scope.add = function() {    
+    Lists.add($scope.list);
+    self.init();
+  };
+
+  $scope.archive = function(list) {
+    Lists.remove(list);
+  };
+
+  $scope.remove = function(list) {
+    Lists.remove(list);
+  };
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+.controller('ListDetailCtrl', function($scope, $stateParams, Lists) {
+  var self = {
+    init: function() {
+      $scope.list = Lists.get($stateParams.listId);
+      $scope.product = {};
+    }
+  };
+  self.init();  
+
+  $scope.add = function() {    
+    $scope.list.products.push($scope.product);
+    self.init();
+  };
+
+  $scope.archive = function(list) {
+    Lists.remove(list);
+  };
+
+  $scope.remove = function(list) {
+    Lists.remove(list);
+  };
 })
 
 .controller('FriendsCtrl', function($scope, Friends) {
--- www/js/services.js
+++ www/js/services.js
@@ -1,47 +1,40 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
-
-  // Some fake testing data
-  var chats = [{
+.factory('Lists', function() {
+  var lists = [{
     id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+    name: 'Semanal',
+    products: [{id: 0, listId: 0, name: 'Batata', checked: false}]
   }, {
     id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Andrew Jostlin',
-    lastText: 'Did you get the ice cream?',
-    face: 'https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg'
-  }, {
-    id: 3,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 4,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg'
+    name: 'Churrasco',
+    products: [{id: 1, listId: 1, name: 'Picanha', checked: true}]
   }];
 
   return {
     all: function() {
-      return chats;
+      return lists;
     },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
+    remove: function(list) {
+      lists.splice(lists.indexOf(list), 1);
     },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+    add: function(list) {
+      var id;
+      var last = lists.length - 1;
+      if(last < 0) {
+        id = 0;
+      }
+      else {
+        id = lists[last].id + 1;
+      }
+
+      list.id = id;
+      lists.push(list);
+    },
+    get: function(listId) {
+      for (var i in lists) {
+        if (lists[i].id == listId) {
+          return lists[i];
         }
       }
       return null;
